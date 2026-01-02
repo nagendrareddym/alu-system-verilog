7c89cfb5379c8923caf162161bf065bff53c41a2
nagendra reddy middi <nagendrareddy.m46@gmail.com>
2026-01-02 12:10:44 +0530

    add to env

---
+++ env/generator.sv
@@ -0,0 +1,20 @@
+class generator ;
+  transaction trans;
+  
+  
+  mailbox gen2drv;
+  function new (mailbox gen2drv);
+    
+    this.gen2drv=gen2drv;
+  endfunction
+  
+  task main();
+    repeat(2) begin
+      trans=new();
+      trans.randomize();
+      $display("generator class signal");
+      
+      gen2drv.put(trans);
+      end
+  endtask
+    endclass
